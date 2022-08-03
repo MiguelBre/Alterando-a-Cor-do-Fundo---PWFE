8ab281e4560ad695db1a9c0bce680280d58231f2
MiguelBre <migbress12@gmail.com>
2022-08-03 10:57:10 -0300

    .

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function mudarCor (){
+    const nomeCor = document.getElementById('cor').value.toLowerCase();
+    let cor = '#FFFFFF';
+    if(nomeCor == 'vermelho'){
+        cor = '#FF0000';
+    } else if (nomeCor == 'verde'){
+        cor = '#00FF00';
+    } else if (nomeCor == 'azul'){
+        cor = '#0000FF';
+    } else if (nomeCor == 'amarelo'){
+        cor = '#FFFF00';
+    }
+
+    //document.body.style.backgroundColor = `#${cor}`;
+    document.documentElement.style.setProperty('--bg-color', `${cor}`);
+
+
+}
+
+document.getElementById('aplicar').addEventListener('click', mudarCor);
